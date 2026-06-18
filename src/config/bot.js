d7764a33b351391a75237fa9071637ab5549fabc
kingkrazy124 <dragonwatch100@gmail.com>
2026-06-18 13:17:56 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Working for Manhatten Roleplay",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#f75414",
+      secondary: "#7d7d7d",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
